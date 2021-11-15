1fd21e9087f2ecda2c30e3947bf156db65132842
Matthew Goodman <mattgoodman95@gmail.com>
2021-11-15 17:33:31 +0000

    add basic content

--- react-app/src/IntroContent.jsx
+++ react-app/src/IntroContent.jsx
@@ -4,19 +4,8 @@ import React from "react";
 
 const IntroContent = () => (
   <div className="intro-content">
-    <h1 className="title">The 0.7% Commitment</h1>
-    <p className="intro-para">
-      The 2019 Conservative Manifesto declared Britain would{" "}
-      <strong>
-        "proudly maintain our commitment to spend 0.7 per cent of GNI on
-        development"
-      </strong>
-      . But just one year later, the government intends to cut foreign aid.
-      <br></br>
-      <br></br> At a time of unprecedented international crisis, with millions
-      at risk of extreme poverty,{" "}
-      <strong>Britain must show leadership - not break its commitments.</strong>
-    </p>
+    <h1 className="title">Vaccine Campaign</h1>
+    <h2> Contact your MP Today: Vaccine Access is a Human Right</h2>
   </div>
 );
 export default IntroContent;
