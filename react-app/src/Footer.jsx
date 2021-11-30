9974e427563edc1e686140f8103331f3c2f9161d
mattg95 <mattgoodman95@gmail.com>
2021-11-30 14:24:52 +0000

    edit links

--- react-app/src/Footer.jsx
+++ react-app/src/Footer.jsx
@@ -9,9 +9,9 @@ const Footer = () => (
           <a href="https://nomorepandemics.com/">
             Find out about No More Pandemics
           </a>
-          <a href="https://nomorepandemics.com/?page_id=102">Who We Are</a>
-          <a href="https://nomorepandemics.com/?page_id=104">How we work</a>
-          <a href="https://nomorepandemics.com/?page_id=12">Contact Us</a>
+          <a href="https://nomorepandemics.com/who-we-are">Who We Are</a>
+          <a href="https://nomorepandemics.com/how-we-work">How we work</a>
+          <a href="https://nomorepandemics.com/contact">Contact Us</a>
           © 2021 No More Pandemics
           <br />
         </Col>
